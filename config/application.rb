c6b01f0f7e91439d62c4bbcc56f93dfdc369b7be
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-05-14 15:53:01 +0300

    Fix rubocop offenses

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,16 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module Applogic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -29,5 +29,7 @@ module Applogic
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.api_only = true
   end
 end
