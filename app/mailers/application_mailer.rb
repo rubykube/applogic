ad9ec7ba0be7dcc5ad21f85a694f5b32c0b21691
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-05-21 18:38:51 +0300

    SENDER_MAIL costumization

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV.fetch('SENDER_EMAIL', 'noreply@rubykube.io')
   layout 'mailer'
 end
