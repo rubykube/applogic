6258fd53fb3f78969b17221f2535b266c4eb8159
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-05-22 18:33:37 +0300

    Account Created Consumer spec

---
+++ spec/consumers/barong/model/account_created_consumer_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Barong::Model::AccountCreatedConsumer do
+  let!(:user) { create(:user) }
+
+  describe '.call' do
+    let(:event) do
+      {
+        name: 'model.account.created',
+        record: {
+          email: 'email@example.com',
+          confirmation_token: 'n1Ytj6Hy57YpfueA2vtmnwJQs583bpYn7Wsfr'
+        }
+      }
+    end
+
+    subject(:call) { described_class.call(event) }
+
+    before do
+      allow(BarongMailer).to receive_message_chain(:verification_email, :deliver_now)
+    end
+
+    it 'triggers the verification email mailer' do
+      expect(BarongMailer).to receive(:verification_email)
+                                 .with('email@example.com', 'n1Ytj6Hy57YpfueA2vtmnwJQs583bpYn7Wsfr')
+      call
+    end
+  end
+end
