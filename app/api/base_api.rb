37f4e6979ef972e4943ee4b3f2e85e8acfed33b3
Dmytro V <staff1489@gmail.com>
2018-06-08 14:20:37 +0300

    #1: remove frontend prefix from grape routes

--- app/api/base_api.rb
+++ app/api/base_api.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class BaseAPI < Grape::API
-  # Prefix will be added in a route after /api
-  prefix 'frontend'
-
   # Use endpoints of all available versions
   # Use Accept header to reach the specific version.
   # E.g. Accept:application/vnd.frontend-v1+json
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount BaseAPI, at: BaseAPI.prefix
+  mount BaseAPI, at: '/'
 end
