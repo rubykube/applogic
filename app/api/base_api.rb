ef51fcb3d59b6c6fe35780e22a67d28ad40928f8
Dmytro V <staff1489@gmail.com>
2018-06-08 14:22:02 +0300

    #1: remove v2 of the api

--- app/api/base_api.rb
+++ app/api/base_api.rb
@@ -2,10 +2,7 @@
 
 class BaseAPI < Grape::API
   # Use endpoints of all available versions
-  # Use Accept header to reach the specific version.
-  # E.g. Accept:application/vnd.frontend-v1+json
   namespace :api do
     mount V1::Base
-    mount V2::Base
   end
 end
