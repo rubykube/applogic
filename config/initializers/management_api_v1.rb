dfa14a35de6965b3b154f4654cc23c0f8652e7ab
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-06-11 12:43:06 +0300

    squash peatio & barong management API config

--- config/initializers/management_api_v1.rb
+++ config/initializers/management_api_v1.rb
@@ -1,38 +1,18 @@
 require 'yaml'
 require 'openssl'
 
-%w[peatio barong].each do |application|
-  (YAML.load_file("config/#{application}_management_api_v1.yml") || {}).deep_symbolize_keys.tap do |x|
-    x.fetch(:keychain).each do |id, key|
-      x[:keychain][id][:value] = OpenSSL::PKey.read(Base64.urlsafe_decode64(key.fetch(:value)))
-    end
-
-    x.fetch(:actions).each do |action_name, action_settings|
-      action_settings[:required_signatures].map!(&:to_sym)
-      if action_settings[:required_signatures].empty?
-        raise ArgumentError, "actions.#{action_name}.required_signatures is empty, " \
-                             "however it should contain at least one value (in config/#{application}_management_api_v1.yml)."
-      end
-    end
-
-    Rails.configuration.x.public_send "#{application}_management_api_v1_configuration=", x
+(YAML.load_file('config/management_api_v1.yml') || {}).deep_symbolize_keys.each do |k, v|
+  v.fetch(:keychain).each do |keychain_id, keychain_key|
+    v[:keychain][keychain_id][:value] = OpenSSL::PKey.read(Base64.urlsafe_decode64(keychain_key.fetch(:value)))
   end
-end
 
-# (YAML.load_file("config/management_api_v1.yml") || {}).deep_symbolize_keys.tap do |x|
-#   x.each do |application|
-#   x.fetch(:keychain).each do |id, key|
-#     x[:keychain][id][:value] = OpenSSL::PKey.read(Base64.urlsafe_decode64(key.fetch(:value)))
-#   end
-#
-#   x.fetch(:actions).each do |action_name, action_settings|
-#     action_settings[:required_signatures].map!(&:to_sym)
-#     if action_settings[:required_signatures].empty?
-#       raise ArgumentError, "actions.#{action_name}.required_signatures is empty, " \
-#                            "however it should contain at least one value (in config/#{application}_management_api_v1.yml)."
-#     end
-#   end
-#
-#   Rails.configuration.x.public_send "#{application}_management_api_v1_configuration=", x
-#   end
-# end
+  v.fetch(:actions).each do |action_name, action_settings|
+    action_settings[:required_signatures].map!(&:to_sym)
+    if action_settings[:required_signatures].empty?
+      raise ArgumentError, "actions.#{action_name}.required_signatures is empty, " \
+                           'however it should contain at least one value (in config/management_api_v1.yml).'
+    end
+  end
+
+  Rails.configuration.x.public_send "#{k}_management_api_v1_configuration=", v
+end
